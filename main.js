11a3eff5637859db8539a3176d8cdced0c5f3f04
GauravVKulkarni <gauravvilaskulkarni9211@gmail.com>
2024-04-01 12:19:58 +0530

    Calculate sum

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-const message = "Hello!";
-console.log(message);
+function calcSum(){
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+
+    if (!isNaN(num1) && !isNaN(num2)){
+        var sum = num1 + num2;
+        document.getElementById('sum').innerHTML = sum;
+    }
+    else{
+        document.getElementById('sum').innerHTML = "Enter valid input";
+    }
+}
