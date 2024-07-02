40d93c0d400b063a3856532e53a5cf89be6605c1
GauravVKulkarni <92051349+GauravVKulkarni@users.noreply.github.com>
2024-07-02 23:50:15 +0530

    JS day 3

---
+++ 02-dataTypes.js
@@ -0,0 +1,15 @@
+int = 16
+num = int + "e"
+console.log(num)
+
+let a = 10
+let b = "hello"
+let c = true
+let d = 12.444
+
+console.table([a,b,c,d])
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
