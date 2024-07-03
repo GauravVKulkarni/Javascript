1166dc7d35e0e84a802ce5ca9f90e2485eb54f72
GauravVKulkarni <92051349+GauravVKulkarni@users.noreply.github.com>
2024-07-04 01:45:30 +0530

    JS day 3.2

---
+++ 03-strings.js
@@ -0,0 +1,10 @@
+let str1 = `hello`
+const str2 = new String(`hii`)
+console.log(`${str1} and ${str2}`)
+console.log(typeof str1)
+console.log(typeof str2)
+
+typeOfStr1 = typeof str1
+typeOfStr2 = typeof str2
+
+console.log(`${typeOfStr1} and ${typeOfStr2}`)
