a33a69f6bcfac0e91a8d01d76e3e261edae1a1d6
GauravVKulkarni <92051349+GauravVKulkarni@users.noreply.github.com>
2024-07-10 23:28:47 +0530

    .

---
+++ 04-numbers.js
@@ -0,0 +1,24 @@
+const numberOne = 100
+const numberTwo = new Number(200)
+
+console.log(typeof(numberOne))
+console.log(typeof(numberTwo))
+
+const numberThree = 123.456
+console.log(numberThree.toFixed(2))
+console.log(numberThree.toPrecision(2))
+
+const numberFour = 10000000
+console.log(numberFour.toExponential(2))
+console.log(numberFour.toLocaleString())
+console.log(numberFour.toLocaleString(`en-in`))
+
+console.log(Math.pow(2,3))
+
+console. log (Math)
+console. log (Math.abs (-65))
+console. log (Math. round (47.6))
+console. log (Math.ceil(5.2))
+console. log (Math.floor (4.9));
+
+console.log(Math.random())
